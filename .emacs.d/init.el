035928c8794337c82067eaaa2bde040d617acacf
clngn <clngn.cc@gmail.com>
2012-08-03 23:39:42 +0900

    fix ac comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -231,9 +231,12 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/dict")
 (global-auto-complete-mode t)
 (ac-config-default)
-(setq ac-dwim t)  ; 補完時にTABをRETの挙動に
-(setq ac-auto-start 3)  ; 3文字以上入力で補完開始
-(setq ac-use-menu-map t)  ; メニュー表示時のみ有効なキーマップを設定
+;; 補完時にTABをRETの挙動に
+(setq ac-dwim t)
+;; 3文字以上入力で補完開始
+(setq ac-auto-start 3)
+;; メニュー表示時のみ有効なキーマップを設定
+(setq ac-use-menu-map t)
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
 
