d22634c752e95421f4cbacdb784f69572a72abe3
clngn <clngn.cc@gmail.com>
2012-09-24 13:43:29 +0900

    fix python setting

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.zshrc .vimrc .vim .tmux.conf .pythonrc.py .gitconfig)
+DOT_FILES=(.zshrc .vimrc .vim .tmux.conf .pythonrc.py .gitconfig .gitignore)
 
 for file in ${DOT_FILES[@]}
 do
