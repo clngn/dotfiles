5ab5f7b7e59e6736f21ea63781d2281efe7a8547
clngn <clngn.cc@gmail.com>
2012-10-25 20:44:03 +0900

    add powerline.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,6 +109,9 @@
 ;; メニューバーを表示
 (menu-bar-mode t)
 
+;; スクロールバーを右側に表示
+(set-scroll-bar-mode 'right)
+
 ;; バックアップファイルの無効
 (setq make-backup-files nil)
 
@@ -470,3 +473,118 @@
              ))
 (setq tex-command "~/.emacs.d/platex2pdf") ; スクリプトを使って直接pdfにコンパイル
 (setq dvi2-command "open -a Preview.app")  ; プレビュー.appでpdfを開く
+
+
+;; powerline
+(require 'powerline)
+(defun arrow-right-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 18 2 1\",
+\".	c %s\",
+\" 	c %s\",
+\".           \",
+\"..          \",
+\"...         \",
+\"....        \",
+\".....       \",
+\"......      \",
+\".......     \",
+\"........    \",
+\".........   \",
+\".........   \",
+\"........    \",
+\".......     \",
+\"......      \",
+\".....       \",
+\"....        \",
+\"...         \",
+\"..          \",
+\".           \"};"  color1 color2))
+
+(defun arrow-left-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 18 2 1\",
+\".	c %s\",
+\" 	c %s\",
+\"           .\",
+\"          ..\",
+\"         ...\",
+\"        ....\",
+\"       .....\",
+\"      ......\",
+\"     .......\",
+\"    ........\",
+\"   .........\",
+\"   .........\",
+\"    ........\",
+\"     .......\",
+\"      ......\",
+\"       .....\",
+\"        ....\",
+\"         ...\",
+\"          ..\",
+\"           .\"};"  color2 color1))
+
+(defconst color1 "#8b7258")
+(defconst color2 "#6f5b46")
+(defconst color3 "#544535")
+(defconst color4 "#372d23")
+
+(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 'xpm t :ascent 'center))
+(defvar arrow-right-2 (create-image (arrow-right-xpm color2 color3) 'xpm t :ascent 'center))
+(defvar arrow-right-3 (create-image (arrow-right-xpm color3 "None") 'xpm t :ascent 'center))
+(defvar arrow-left-1  (create-image (arrow-left-xpm color2 color1) 'xpm t :ascent 'center))
+(defvar arrow-left-2  (create-image (arrow-left-xpm color3 color2) 'xpm t :ascent 'center))
+(defvar arrow-left-3  (create-image (arrow-left-xpm "None" color3) 'xpm t :ascent 'center))
+
+(setq-default mode-line-format
+              (list
+               '(:eval (concat (propertize " %Z%* " 'face 'mode-line-color-1)
+                               (propertize " " 'display arrow-right-1)))
+               '(:eval (concat (propertize " %b " 'face 'mode-line-color-2)
+                               (propertize " " 'display arrow-right-2)))
+               '(:eval (concat (propertize " %m" 'face 'mode-line-color-3)
+                               (powerline-minor-modes 'left color3)
+                               (propertize " " 'face 'mode-line-color-3)
+                               (propertize " " 'display arrow-right-3)))
+
+               ;; Justify right by filling with spaces to right fringe - 16
+               ;; (16 should be computed rahter than hardcoded)
+               '(:eval (propertize " " 'display '((space :align-to (- right-fringe 20)))))
+
+               '(:eval (concat (propertize " " 'display arrow-left-3)
+                               (propertize " " 'face 'mode-line-color-3)
+                               (powerline-vc 'center color3)
+                               (propertize " " 'face 'mode-line-color-3)))
+               '(:eval (concat (propertize " " 'display arrow-left-2)
+                               (propertize " %p " 'face 'mode-line-color-2)))
+               '(:eval (concat (propertize " " 'display arrow-left-1)
+                               (propertize "%4l:%2c  " 'face 'mode-line-color-1)))
+               ))
+
+(make-face 'mode-line-color-1)
+(set-face-attribute 'mode-line-color-1 nil
+                    :foreground "#fff"
+                    :background color1)
+
+(make-face 'mode-line-color-2)
+(set-face-attribute 'mode-line-color-2 nil
+                    :foreground "#fff"
+                    :background color2)
+
+(make-face 'mode-line-color-3)
+(set-face-attribute 'mode-line-color-3 nil
+                    :foreground "#fff"
+                    :background color3)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fff"
+                    :background color4
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :foreground "#fff"
+                    :background color4)
