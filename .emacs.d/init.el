e19e96264fe7fc6410af840ff6678db25379a4c9
clngn <clngn.cc@gmail.com>
2012-10-28 21:49:58 +0900

    add lessc command

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -427,6 +427,7 @@
 (autoload 'less-css-mode "less-css-mode" nil t)
 (setq auto-mode-alist
       (cons '("\\.less\\'" . less-css-mode) auto-mode-alist))
+(setq exec-path (append (list (expand-file-name "~/.nodebrew/current/bin")) exec-path))
 (add-to-list 'ac-modes 'less-css-mode)  ; ac-mode
 (add-hook 'less-css-mode-hook
           '(lambda()
