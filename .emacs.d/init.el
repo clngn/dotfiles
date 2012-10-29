e5ff3d18ce341b908ff1faa61ad55e41970b6303
clngn <clngn.cc@gmail.com>
2012-10-29 09:24:15 +0900

    fix yasnippet setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -300,8 +300,25 @@
 
 ;; yasnippet
 (require 'yasnippet)
-(yas--initialize)
-(yas/load-directory "~/.emacs.d/snippets")
+(yas-global-mode 1)
+;; anything interface
+;; http://d.hatena.ne.jp/sugyan/20120101/1325429637
+(eval-after-load "anything-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (anything-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*anything yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))))
 
 ;; anything
 (require 'anything-startup)
