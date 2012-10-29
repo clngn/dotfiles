3e81d67b2eeca5232dca11f4b8994cefee44815a
clngn <clngn.cc@gmail.com>
2012-10-29 09:35:31 +0900

    fix gitignore

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -68,7 +68,7 @@
               "/usr/bin"
               "/usr/local/bin"
               (expand-file-name "~/bin")  ; 絶対パスに展開
-              (expand-file-name "~/.emacs/bin")
+              (expand-file-name "~/.emacs.d/bin")
               ))
   ;; PATHとexec-pathに同じものを追加
   (when (and (file-exists-p dir) (not (member dir exec-path)))
