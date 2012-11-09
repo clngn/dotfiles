931c11cc02622a58cf7f4316c0106152cb74a73a
clngn <clngn.cc@gmail.com>
2012-11-09 19:11:57 +0900

    fix elscreen-tab-display-kill-screen hidden

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -260,6 +260,7 @@
 ;; elscreen
 (require 'elscreen)
 (elscreen-start)
+(setq elscreen-tab-display-kill-screen nil)  ; タブ左端のxを非表示
 (global-set-key (kbd "M-t") 'elscreen-create)  ; 新規タブ
 (global-set-key (kbd "M-T") 'elscreen-clone)   ; 新規タブでカレントバッファ引継ぎ
 (global-set-key (kbd "C-<tab>") 'elscreen-next)        ; タブ移動
