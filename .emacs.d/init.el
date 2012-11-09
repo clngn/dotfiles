8bf46139835e711327e3e66403962f0570940178
clngn <clngn.cc@gmail.com>
2012-11-09 19:12:30 +0900

    change tramp auto-save-directory

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -239,6 +239,7 @@
 
 ;; tramp
 (require 'tramp)
+(setq tramp-auto-save-directory "/tmp")
 (add-to-list 'backup-directory-alist
              (cons tramp-file-name-regexp nil))  ; 自動でバックアップファイルをつくらない
 
