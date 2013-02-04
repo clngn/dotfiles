c214e66b6ce573793dec62233c2c8c4a6c1225b0
clngn <clngn.cc@gmail.com>
2013-02-04 23:21:21 +0900

    commentout popwin-yatex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -399,6 +399,8 @@
 ;; popwin
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
+;; (require 'popwin-yatex)
+;; (push '("*YaTeX-typesetting*") popwin:special-display-config)
 
 
 ;-------------------------------------------------------------------------------
