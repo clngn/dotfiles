beef46fea60183b4e3be8ec5980a5f087c991e3d
clngn <clngn.cc@gmail.com>
2013-07-02 00:05:50 +0900

    add coffee-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -503,6 +503,14 @@
              (setq yas/mode-symbol 'js-mode)
              ))
 
+;; coffee-mode
+(autoload 'coffee-mode "coffee-mode" nil t)
+(setq auto-mode-alist
+      (cons '("\\.coffee$" . coffee-mode) auto-mode-alist))
+(add-hook 'coffee-mode-hook
+          '(lambda ()
+             ))
+
 ;; php-mode
 (autoload 'php-mode "php-mode" nil t)
 ;; (setq auto-mode-alist
