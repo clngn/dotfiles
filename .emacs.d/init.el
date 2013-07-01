7bdf53c0f9c144b93b3dee4eab89b474ce5e0cd7
clngn <clngn.cc@gmail.com>
2013-07-02 00:05:31 +0900

    update elscreen from package.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -306,7 +306,8 @@
 (package-initialize)
 
 ;; elscreen
-(require 'elscreen)
+;; (require 'elscreen)
+(when (>= emacs-major-version 24)
 (elscreen-start)
 (setq elscreen-tab-display-kill-screen nil)  ; タブ左端のxを非表示
 (setq elscreen-display-screen-number nil)  ; モードラインでの表示を非表示
@@ -314,6 +315,7 @@
 (global-set-key (kbd "M-T") 'elscreen-clone)   ; 新規タブでカレントバッファ引継ぎ
 (global-set-key (kbd "C-<tab>") 'elscreen-next)        ; タブ移動
 (global-set-key (kbd "C-S-<tab>") 'elscreen-previous)  ; タブ移動
+)
 
 ;; auto-complete
 (require 'auto-complete-config)
