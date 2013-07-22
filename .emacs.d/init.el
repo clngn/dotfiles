bd160488389e3c625e5999811762d58938da9cec
clngn <clngn.cc@gmail.com>
2013-07-23 01:25:35 +0900

    change anything to helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -359,6 +359,16 @@
   (message "done."))
 (add-hook 'find-file-not-found-hooks 'auto-insert)
 
+;; helm
+(require 'helm-config)
+(global-set-key (kbd "C-x b") 'helm-for-files)
+(global-set-key (kbd "M-r") 'helm-resume)
+
+;; anything
+;; (require 'anything-startup)
+;; (global-set-key (kbd "C-;") 'anything-command-map)
+;; (global-set-key (kbd "C-x b") 'anything-for-files)
+
 ;; yasnippet
 (require 'yasnippet)
 (yas-global-mode 1)
@@ -366,28 +376,26 @@
 (yas/load-directory yas/root-directory)
 ;; anything interface
 ;; http://d.hatena.ne.jp/sugyan/20120101/1325429637
-(eval-after-load "anything-config"
-  '(progn
-     (defun my-yas/prompt (prompt choices &optional display-fn)
-       (let* ((names (loop for choice in choices
-                           collect (or (and display-fn (funcall display-fn choice))
-                                       choice)))
-              (selected (anything-other-buffer
-                         `(((name . ,(format "%s" prompt))
-                            (candidates . names)
-                            (action . (("Insert snippet" . (lambda (arg) arg))))))
-                         "*anything yas/prompt*")))
-         (if selected
-             (let ((n (position selected names :test 'equal)))
-               (nth n choices))
-           (signal 'quit "user quit!"))))
-     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
-     (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
-
-;; anything
-(require 'anything-startup)
-(global-set-key (kbd "C-;") 'anything-command-map)
-(global-set-key (kbd "C-x b") 'anything-for-files)
+;; (eval-after-load "anything-config"
+;;   '(progn
+;;      (defun my-yas/prompt (prompt choices &optional display-fn)
+;;        (let* ((names (loop for choice in choices
+;;                            collect (or (and display-fn (funcall display-fn choice))
+;;                                        choice)))
+;;               (selected (anything-other-buffer
+;;                          `(((name . ,(format "%s" prompt))
+;;                             (candidates . names)
+;;                             (action . (("Insert snippet" . (lambda (arg) arg))))))
+;;                          "*anything yas/prompt*")))
+;;          (if selected
+;;              (let ((n (position selected names :test 'equal)))
+;;                (nth n choices))
+;;            (signal 'quit "user quit!"))))
+;;      (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+;;      (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
+;; helm-c-yasnippet
+(require 'helm-c-yasnippet)
+(global-set-key (kbd "C-c y") 'helm-c-yas-complete)
 
 ;; org-remember
 (setq org-startup-truncated nil)
