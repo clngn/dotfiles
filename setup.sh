e24e984428f1c7c62a7921ee4e7777d024043fb6
clngn <clngn.cc@gmail.com>
2014-01-26 22:34:40 +0900

    add .emacs.d directory for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=(.zshrc .vimrc .vim .tmux.conf .pythonrc.py .gitconfig .gitignore)
+DOT_FILES=(.zshrc .vimrc .vim .tmux.conf .pythonrc.py .gitignore .gitconfig .emacs.d)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
-
