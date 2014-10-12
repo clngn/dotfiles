0d03fc57a937e4799a7ea783ec45ca7723737d02
clngn <clngn.cc@gmail.com>
2014-10-13 01:04:59 +0900

    fix helm config

--- .emacs.d/conf/20-helm.el
+++ .emacs.d/conf/20-helm.el
@@ -1,5 +1,14 @@
 ;; helm
-(require 'helm-config)
-(global-set-key (kbd "C-x b") 'helm-for-files)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-r") 'helm-resume)
+(when (require 'helm-config nil t)
+  (global-set-key (kbd "C-x b") 'helm-for-files)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "M-r") 'helm-resume)
+
+  ;; C-h をBackspaceに割当て
+;  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+;  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+
+  ;; TABで補完
+;  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+)
