0ea50f68cf6f010275fe0b774c43070e857fc256
clngn <clngn.cc@gmail.com>
2018-02-18 10:19:55 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-DOT_FILES=(.atom .zshrc .vimrc .vim .tmux.conf .pythonrc.py .gitignore .gitconfig .emacs.d)
+#DOT_FILES=(.atom .zshrc .vimrc .vim .tmux.conf .pythonrc.py .gitignore .gitconfig .emacs.d)
+DOT_FILES=(.zshrc .vimrc .vim .tmux.conf .gitignore .gitconfig)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+cd ~/.atom
+mv styles.less styles.org.less
+ln -s ~/dotfiles/.atom/styles.less ~/.atom
+mv config.cson config.org.cson
+ln -s ~/dotfiles/.atom/config.cson ~/.atom
+mv keymap.cson keymap.org.cson
+ln -s ~/dotfiles/.atom/keymap.cson ~/.atom
+mv init.coffee init.org.coffee
+ln -s ~/dotfiles/.atom/init.coffee ~/.atom
+mv snippets.cson snippets.org.cson
+ln -s ~/dotfiles/.atom/snippets.cson ~/.atom
+
